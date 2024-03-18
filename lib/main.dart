46dc46be49e6e649663f68c7d5e4bf832edbcdce
AniKusuma <badriyahanikusumaputri@gmail.com>
2024-03-18 15:41:29 +0800

    border

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.lightBlue,
-          appBar: AppBar(
-            title: Text("Aplikasiku"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text("Aplikasiku"),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white, // warna border
+                width: 2.0, // lebar border
+              ),
+            ),
+            child: Text(
+              "HALLO SAYA ANI",
+              style: TextStyle(
+                color: Colors.white, // warna teks
+                fontSize: 20.0, // ukuran teks
+              ),
+            ),
           ),
-          body: Center(
-              child: Text(
-            "HALLO SAYA ANI",
-          )),
-        ));
+        ),
+      ),
+    );
   }
 }
